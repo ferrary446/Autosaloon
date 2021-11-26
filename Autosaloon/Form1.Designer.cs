bef1c1187ee48fbff906292a4b58535eacc67090
krist <krist@LAPTOP-O1VFPKF2>
2021-11-27 02:51:45 +0300

    Добавьте файлы проекта.

---
+++ Autosaloon/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace Autosaloon
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rjButton1 = new CustomBox.RJControls.RJButton();
+            this.rjComboBox1 = new CustomBox.RJControls.RJComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rjComboBox2 = new CustomBox.RJControls.RJComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.rjComboBox3 = new CustomBox.RJControls.RJComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.rjComboBox4 = new CustomBox.RJControls.RJComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.rjProgressBar1 = new CustomBox.RJControls.RJProgressBar();
+            this.SuspendLayout();
+            // 
+            // rjButton1
+            // 
+            this.rjButton1.BackColor = System.Drawing.Color.Gold;
+            this.rjButton1.BackgroundColor = System.Drawing.Color.Gold;
+            this.rjButton1.BorderColor = System.Drawing.Color.Black;
+            this.rjButton1.BorderRadius = 20;
+            this.rjButton1.BorderSize = 1;
+            this.rjButton1.FlatAppearance.BorderSize = 0;
+            this.rjButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.rjButton1.Font = new System.Drawing.Font("Impact", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.rjButton1.ForeColor = System.Drawing.Color.Black;
+            this.rjButton1.Location = new System.Drawing.Point(201, 328);
+            this.rjButton1.Name = "rjButton1";
+            this.rjButton1.Size = new System.Drawing.Size(143, 37);
+            this.rjButton1.TabIndex = 0;
+            this.rjButton1.Text = "Show";
+            this.rjButton1.TextColor = System.Drawing.Color.Black;
+            this.rjButton1.UseVisualStyleBackColor = false;
+            // 
+            // rjComboBox1
+            // 
+            this.rjComboBox1.BackColor = System.Drawing.Color.Gold;
+            this.rjComboBox1.BorderColor = System.Drawing.Color.Gold;
+            this.rjComboBox1.BorderSize = 1;
+            this.rjComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDown;
+            this.rjComboBox1.Font = new System.Drawing.Font("Impact", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.rjComboBox1.ForeColor = System.Drawing.Color.Black;
+            this.rjComboBox1.IconColor = System.Drawing.Color.Black;
+            this.rjComboBox1.ListBackColor = System.Drawing.Color.DarkSlateBlue;
+            this.rjComboBox1.ListTextColor = System.Drawing.Color.WhiteSmoke;
+            this.rjComboBox1.Location = new System.Drawing.Point(292, -1);
+            this.rjComboBox1.MinimumSize = new System.Drawing.Size(200, 30);
+            this.rjComboBox1.Name = "rjComboBox1";
+            this.rjComboBox1.Padding = new System.Windows.Forms.Padding(1);
+            this.rjComboBox1.Size = new System.Drawing.Size(251, 54);
+            this.rjComboBox1.TabIndex = 1;
+            this.rjComboBox1.Texts = "";
+            this.rjComboBox1.OnSelectedIndexChanged += new System.EventHandler(this.RjComboBox1_OnSelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Gold;
+            this.label1.Font = new System.Drawing.Font("Impact", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(-2, -1);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(13);
+            this.label1.Size = new System.Drawing.Size(307, 54);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Please choose the car brand";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // rjComboBox2
+            // 
+            this.rjComboBox2.BackColor = System.Drawing.Color.Gold;
+            this.rjComboBox2.BorderColor = System.Drawing.Color.Gold;
+            this.rjComboBox2.BorderSize = 1;
+            this.rjComboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDown;
+            this.rjComboBox2.Font = new System.Drawing.Font("Impact", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.rjComboBox2.ForeColor = System.Drawing.Color.Black;
+            this.rjComboBox2.IconColor = System.Drawing.Color.Black;
+            this.rjComboBox2.ListBackColor = System.Drawing.Color.DarkSlateBlue;
+            this.rjComboBox2.ListTextColor = System.Drawing.Color.WhiteSmoke;
+            this.rjComboBox2.Location = new System.Drawing.Point(292, 53);
+            this.rjComboBox2.MinimumSize = new System.Drawing.Size(200, 30);
+            this.rjComboBox2.Name = "rjComboBox2";
+            this.rjComboBox2.Padding = new System.Windows.Forms.Padding(1);
+            this.rjComboBox2.Size = new System.Drawing.Size(251, 54);
+            this.rjComboBox2.TabIndex = 1;
+            this.rjComboBox2.Texts = "";
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Gold;
+            this.label2.Font = new System.Drawing.Font("Impact", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(-2, 161);
+            this.label2.Name = "label2";
+            this.label2.Padding = new System.Windows.Forms.Padding(13);
+            this.label2.Size = new System.Drawing.Size(295, 54);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Please choose location";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // rjComboBox3
+            // 
+            this.rjComboBox3.BackColor = System.Drawing.Color.Gold;
+            this.rjComboBox3.BorderColor = System.Drawing.Color.Gold;
+            this.rjComboBox3.BorderSize = 1;
+            this.rjComboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDown;
+            this.rjComboBox3.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.rjComboBox3.ForeColor = System.Drawing.Color.DimGray;
+            this.rjComboBox3.IconColor = System.Drawing.Color.Black;
+            this.rjComboBox3.ListBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(228)))), ((int)(((byte)(245)))));
+            this.rjComboBox3.ListTextColor = System.Drawing.Color.DimGray;
+            this.rjComboBox3.Location = new System.Drawing.Point(292, 107);
+            this.rjComboBox3.MinimumSize = new System.Drawing.Size(200, 30);
+            this.rjComboBox3.Name = "rjComboBox3";
+            this.rjComboBox3.Padding = new System.Windows.Forms.Padding(1);
+            this.rjComboBox3.Size = new System.Drawing.Size(251, 54);
+            this.rjComboBox3.TabIndex = 1;
+            this.rjComboBox3.Texts = "";
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Gold;
+            this.label3.Font = new System.Drawing.Font("Impact", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(-2, 53);
+            this.label3.Name = "label3";
+            this.label3.Padding = new System.Windows.Forms.Padding(13);
+            this.label3.Size = new System.Drawing.Size(307, 54);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Please choose the model";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // rjComboBox4
+            // 
+            this.rjComboBox4.BackColor = System.Drawing.Color.Gold;
+            this.rjComboBox4.BorderColor = System.Drawing.Color.Gold;
+            this.rjComboBox4.BorderSize = 1;
+            this.rjComboBox4.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDown;
+            this.rjComboBox4.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.rjComboBox4.ForeColor = System.Drawing.Color.DimGray;
+            this.rjComboBox4.IconColor = System.Drawing.Color.Black;
+            this.rjComboBox4.ListBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(228)))), ((int)(((byte)(245)))));
+            this.rjComboBox4.ListTextColor = System.Drawing.Color.DimGray;
+            this.rjComboBox4.Location = new System.Drawing.Point(292, 161);
+            this.rjComboBox4.MinimumSize = new System.Drawing.Size(200, 30);
+            this.rjComboBox4.Name = "rjComboBox4";
+            this.rjComboBox4.Padding = new System.Windows.Forms.Padding(1);
+            this.rjComboBox4.Size = new System.Drawing.Size(251, 54);
+            this.rjComboBox4.TabIndex = 1;
+            this.rjComboBox4.Texts = "";
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.Gold;
+            this.label4.Font = new System.Drawing.Font("Impact", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(-2, 107);
+            this.label4.Name = "label4";
+            this.label4.Padding = new System.Windows.Forms.Padding(13);
+            this.label4.Size = new System.Drawing.Size(295, 54);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Please choose series";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // rjProgressBar1
+            // 
+            this.rjProgressBar1.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.rjProgressBar1.ChannelColor = System.Drawing.Color.Gold;
+            this.rjProgressBar1.ChannelHeight = 6;
+            this.rjProgressBar1.Font = new System.Drawing.Font("Impact", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.rjProgressBar1.ForeBackColor = System.Drawing.Color.Gold;
+            this.rjProgressBar1.ForeColor = System.Drawing.Color.Black;
+            this.rjProgressBar1.Location = new System.Drawing.Point(-2, 249);
+            this.rjProgressBar1.Name = "rjProgressBar1";
+            this.rjProgressBar1.ShowMaximun = false;
+            this.rjProgressBar1.ShowValue = CustomBox.RJControls.TextPosition.Right;
+            this.rjProgressBar1.Size = new System.Drawing.Size(545, 32);
+            this.rjProgressBar1.SliderColor = System.Drawing.Color.RoyalBlue;
+            this.rjProgressBar1.SliderHeight = 6;
+            this.rjProgressBar1.SymbolAfter = "";
+            this.rjProgressBar1.SymbolBefore = "";
+            this.rjProgressBar1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 28F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.ClientSize = new System.Drawing.Size(543, 377);
+            this.Controls.Add(this.rjProgressBar1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.rjComboBox4);
+            this.Controls.Add(this.rjComboBox3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rjComboBox2);
+            this.Controls.Add(this.rjComboBox1);
+            this.Controls.Add(this.rjButton1);
+            this.Font = new System.Drawing.Font("Century", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "Form1";
+            this.Text = "Autosaloon";
+            this.TransparencyKey = System.Drawing.Color.DarkOliveGreen;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private CustomBox.RJControls.RJButton rjButton1;
+        private CustomBox.RJControls.RJComboBox rjComboBox1;
+        private System.Windows.Forms.Label label1;
+        private CustomBox.RJControls.RJComboBox rjComboBox2;
+        private System.Windows.Forms.Label label2;
+        private CustomBox.RJControls.RJComboBox rjComboBox3;
+        private System.Windows.Forms.Label label3;
+        private CustomBox.RJControls.RJComboBox rjComboBox4;
+        private System.Windows.Forms.Label label4;
+        private CustomBox.RJControls.RJProgressBar rjProgressBar1;
+    }
+}
---
+++ Autosaloon/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Autosaloon
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog=AutoDB;Integrated Security=True");
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand("Select * from autoTable", con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            rjComboBox1.DataSource = ds.Tables[0];
+            rjComboBox1.DisplayMember = "AutoBrand";
+            rjComboBox1.ValueMember = "AutoID";
+        }
+
+        private void RjComboBox1_OnSelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (rjComboBox1.SelectedIndex)
+            {
+                case 0:
+                    rjComboBox2.Items.Clear();
+                    rjComboBox2.Items.Add("A1");
+                    break;
+
+                case 1:
+                    rjComboBox2.Items.Clear();
+                    rjComboBox2.Items.Add("330i");
+                    break;
+
+                default: 
+                    rjComboBox2.Items.Clear();
+                    break;
+            }
+        }
+    }
+}
