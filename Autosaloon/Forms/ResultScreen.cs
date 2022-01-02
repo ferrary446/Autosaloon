2c4546ae9e7137fa12cd03460bb7249b25d412eb
Ilya Yushkov <XYUSI002@studenti.czu.cz>
2022-01-02 16:48:10 +0100

    Final result.

--- Autosaloon/Forms/ResultScreen.cs
+++ Autosaloon/Forms/ResultScreen.cs
@@ -25,7 +25,7 @@ namespace Autosaloon.Forms
             cityLocationText.Text = Result.cityLocation;
             countText.Text = Result.count.ToString();
 
-            autoImage.ImageLocation = @$"C:\Users\ponom\Desktop\Autosaloon_Ilya_Yushkov\Autosaloon\Resources\Cars\" +
+            autoImage.ImageLocation = @$"C:\Users\ponom\Desktop\Autosaloon_Ilya_Yushkov\Autosaloon\Autosaloon\Resources\Cars\" +
                 @$"{autoBrandText.Text}\{autoModelText.Text}.jpg";
         }
         // Navrát v menu hledaní aut
